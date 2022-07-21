b8e885da2219940b5c41b35965d2d479b2cfda33
Dima <protsack.dima25@gmail.com>
2022-07-21 16:59:52 +0300

    Initial Commit

---
+++ GameUsingUIPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameUsingUIPicker
+//
+//  Created by Дмитрий Процак on 21.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
